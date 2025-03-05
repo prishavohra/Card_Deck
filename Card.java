f51190c01c1a894c281b828f4f0a96ed1f51813d
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-05 23:22:59 +0530

    constructors in Card.java

--- Card.java
+++ Card.java
@@ -4,3 +4,11 @@
 public class Card {
     private String rank;
     private String suit;
+
+    // Constructor
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    
