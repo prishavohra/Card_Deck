624f728bd32577e53d08f363d526896be62151db
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-05 23:37:55 +0530

    Initialize Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,7 @@
+// Main.java
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
