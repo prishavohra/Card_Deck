5bc4923f4910fb7be24e755739f5676ede2cc4cf
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-05 23:28:59 +0530

    Create calss Deck

--- Deck.java
+++ Deck.java
@@ -1,1 +1,12 @@
+// Deck.java
 
+import java.util.*;
+
+public class Deck {
+    private ArrayList<Card> deck;
+
+    // Constructor to create and initialize deck
+    public Deck() {
+        deck = new ArrayList<>();
+        createDeck();
+    }
