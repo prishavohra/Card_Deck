e4f0a24e6bd8bf6fbf58be78154eb3f4fe337e1d
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-05 23:29:32 +0530

    Create standard Deck

--- Deck.java
+++ Deck.java
@@ -10,3 +10,15 @@ public class Deck {
         deck = new ArrayList<>();
         createDeck();
     }
+
+    // Creates a standard deck of 52 playing cards
+    public void createDeck() {
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        String[] suits = {"Hearts", "Spades", "Clubs", "Diamonds"};
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+    }
